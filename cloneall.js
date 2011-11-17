0ed4dae62b3db7e2d891dc6edcd41d45af5f9414
Dan Tillberg <dan@sky.(none)>
2011-11-16 22:33:33 -0500

    Making awesome, duh.

--- cloneall.js
+++ cloneall.js
@@ -2,7 +2,11 @@ var jsdom = require('jsdom');
 var request = require('request');
 var url = require('url');
 
-console.dir(process.argv);
+var username = process.argv[2];
+if (!username) {
+  console.log('Usage: node cloneall.js <username>');
+  process.exit();
+}
 request({ uri: 'https://github.com/' + process.argv[2] }, function(err, response, body){
   
 });
