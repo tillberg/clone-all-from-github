e51912eb8c94a65c6ef361831685d4585e85c881
Dan Tillberg <dan@sky.(none)>
2011-11-16 22:28:35 -0500

    Making awesome, duh.

--- cloneall.js
+++ cloneall.js
@@ -2,7 +2,7 @@ var jsdom = require('jsdom');
 var request = require('request');
 var url = require('url');
 
-console.dir(argv);
+console.dir(process.argv);
 request({ uri: 'https://github.com/' + argv[1] }, function(err, response, body){
   
 });
