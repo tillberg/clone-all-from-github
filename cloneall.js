0cc393b26b42015c890e3f985f7bb0b744a47e19
Dan Tillberg <dan@sky.(none)>
2011-11-16 22:35:11 -0500

    Making awesome, duh.

--- cloneall.js
+++ cloneall.js
@@ -9,4 +9,13 @@ if (!username) {
 }
 request({ uri: 'https://github.com/' + process.argv[2] }, function(err, response, body){
   if(err && response.statusCode !== 200){ console.log('Request error.'); }
+  jsdom.env({
+    html: body,
+    scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+  }, function(err, window){
+    //Use jQuery just as in a regular HTML page
+    var $ = window.jQuery;
+    console.log($('title').text());
+    res.end($('title').text());
+  });
 });
